c74e695c1a0f07d14e0bac42de30891bc79c5287
purav-parekh <purav98@gmail.com>
2025-05-23 11:09:16 -0400

    setup langchain workflows - stateless

---
+++ stateless_bot.py
@@ -0,0 +1,26 @@
+import os
+from dotenv import load_dotenv
+from langchain_openai import OpenAIEmbeddings
+from langchain.chains import RetrievalQA
+from langchain_community.chat_models import ChatOpenAI
+from langchain_pinecone import PineconeVectorStore
+
+load_dotenv()
+
+# establishes a connection to existing Pinecone index where the vector representations (embeddings) is stored.
+embeddings = OpenAIEmbeddings()
+vectorstore = PineconeVectorStore(index_name=os.environ.get("INDEX_NAME"), embedding=embeddings)
+
+# langchain workflows
+chat = ChatOpenAI(model="gpt-4.1-nano", temperature=0,verbose=True)
+qa = RetrievalQA.from_chain_type(
+    llm=chat,
+    chain_type="stuff",
+    retriever=vectorstore.as_retriever(),
+)
+
+res = qa.invoke("What is the time limit for each over bowled?")
+print(res)
+
+res = qa.invoke("What are the playing conditions for the toss?")
+print(res)
