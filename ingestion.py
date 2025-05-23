3f1f8dab0f28b99f35dd15da593e91ecd032e0dc
purav-parekh <purav98@gmail.com>
2025-05-23 11:08:33 -0400

    setup chunks & embeddings

---
+++ ingestion.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+
+load_dotenv()
+
+# read pdf
+loader = PyPDFLoader("data/t20-pc.pdf")
+document = loader.load() # it reads the PDF, extracts the text, and returns a list of LangChain documents, ready for further processing.
+
+# create chunks
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=100) # chunk_size = max chars in each chunk, chunk_overlap = chars that overlap between chunks, helps maintain context btw neighboring chunks.
+texts = text_splitter.split_documents(document) # it splits the documents into smaller chunks for better processing.
+print(f"Total chunks: {len(texts)}") # it prints the total number of chunks created from the documents.
+
+# create embeddings
+embeddings = OpenAIEmbeddings(openai_api_key = os.environ.get("OPEN_API_KEY")) # it creates embeddings for the text chunks using OpenAI's embedding model.
+PineconeVectorStore.from_documents(texts, embeddings, index_name=os.environ.get("INDEX_NAME")) # it creates a Pinecone vector store from the text chunks and embeddings, allowing for efficient similarity search and retrieval of relevant information.
